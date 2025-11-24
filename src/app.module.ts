6773d913bb5e4fb74057dffc6476f0939d87f2a8
AreebaMujahid <areebamujahidali55@gmail.com>
2025-11-24 14:18:01 +0500

    eslint fix , which is generic in all projects

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { AuthModule } from './auth/auth.module';
+import { Request, Response } from 'express';
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      introspection: true,
+      playground: true,
+      context: ({ req, res }: { req: Request; res: Response }) => ({
+        req,
+        res,
+      }),
+    }),
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.resolver.ts
@@ -0,0 +1,8 @@
+import { Resolver, Query } from '@nestjs/graphql';
+@Resolver()
+export class AuthResolver {
+  @Query(() => String)
+  hello(): string {
+    return 'Hello, World!';
+  }
+}

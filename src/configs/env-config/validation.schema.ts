a7f629f3115341e85840a5bb51dd5f5b90de3d2d
AreebaMujahid <areebamujahidali55@gmail.com>
2025-12-08 09:14:05 +0500

    deployement

--- src/configs/env-config/validation.schema.ts
+++ src/configs/env-config/validation.schema.ts
@@ -1,11 +1,7 @@
 import * as Joi from 'joi';
 export const validationSchema = Joi.object({
   //db modules validation
-  DB_HOST: Joi.string().required(),
-  DB_PORT: Joi.number().required(),
-  DB_NAME: Joi.string().required(),
-  DB_USERNAME: Joi.string().required(),
-  DB_PASSWORD: Joi.string().allow(''),
+
   DB_SYNC: Joi.boolean().truthy('true').falsy('false').required(),
   DB_SSL: Joi.boolean().truthy('true').falsy('false').required(),
   DATABASE_URL: Joi.string().uri().required(),

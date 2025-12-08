d9e660c7fcc47e97b92e3c487cda26a31ade933f
AreebaMujahid <areebamujahidali55@gmail.com>
2025-12-08 09:36:46 +0500

    deployement

--- src/typeormconfig.ts
+++ src/typeormconfig.ts
@@ -7,8 +7,7 @@ const isProd = process.env.NODE_ENV === 'production';
 export const AppDataSource = new DataSource({
   type: 'postgres',
   url: process.env.DATABASE_URL,
-  ssl:
-    process.env.DATABASE_SSL === 'true' ? { rejectUnauthorized: false } : false,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : false,
   synchronize: false,
   logging: false,
   entities: [User, Crew],

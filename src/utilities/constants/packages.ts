845cd78bbb109fa0881c8cd0455863adde317f71
AreebaMujahid <areebamujahidali55@gmail.com>
2025-12-10 01:14:40 +0500

    feedback implemnt

--- src/utilities/constants/packages.ts
+++ src/utilities/constants/packages.ts
@@ -1,8 +1,1 @@
-export const packages = [
-  'Non-featured',
-  'Basic',
-  'Bronze',
-  'Silver',
-  'Gold',
-  'Platinum',
-];
+export const packages = ['Basic', 'Bronze', 'Silver', 'Gold', 'Platinum'];

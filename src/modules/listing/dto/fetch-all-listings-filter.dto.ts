ddb83be6efd5841a44302cafc0943794cae95b4e
AreebaMujahid <areebamujahidali55@gmail.com>
2025-12-19 08:47:55 +0500

    initial commit

--- src/modules/listing/dto/fetch-all-listings-filter.dto.ts
+++ src/modules/listing/dto/fetch-all-listings-filter.dto.ts
@@ -1,7 +1,11 @@
 import { ObjectType, Field, InputType } from '@nestjs/graphql';
+import { PaginationDto } from 'src/modules/shared/pagination/pagination.input.dto';
 
 @InputType()
 export class FetchAllListingsInput {
   @Field()
   isActive: boolean;
+
+  @Field(() => PaginationDto, { nullable: true })
+  pagination?: PaginationDto;
 }
